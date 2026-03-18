29aac6710b4288a269322d2f3615415f022060df
GibkiMakaron <bartek.wrobel2016@onet.pl>
2026-03-18 12:57:12 +0100

    szkielet urzytkownikow

---
+++ s33323/Uzytkownik.cs
@@ -0,0 +1,31 @@
+﻿namespace s33323;
+
+public abstract class Uzytkownik
+{
+    protected int id;
+    protected string imie;
+    protected string nazwisko;
+    protected string typ;
+}
+
+public class Student : Uzytkownik
+{
+    public Student(int id, string imie, string nazwisko)
+    {
+        this.id = id;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        typ = "student";
+    }
+}
+
+public class Pracownik : Uzytkownik
+{
+    public Pracownik(int id, string imie, string nazwisko)
+    {
+        this.id = id;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        typ = "pracownik";
+    }
+}
