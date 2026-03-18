84f5cc4242a0e6cbfa7f36b34ba78006dc470ba5
GibkiMakaron <bartek.wrobel2016@onet.pl>
2026-03-18 12:57:27 +0100

    szkielet sprzetow

---
+++ s33323/Sprzet.cs
@@ -0,0 +1,49 @@
+﻿namespace s33323;
+
+public abstract class Sprzet
+{
+    protected static int id = 0;
+    protected string typ = "brak";
+}
+
+public class Laptop : Sprzet
+{
+    private int cale;
+    private string rozdzielczosc;
+
+    public Laptop(int cale, string rozdzielczosc)
+    {
+        id = id++;
+        typ = "laptop";
+        this.cale =  cale;
+        this.rozdzielczosc =  rozdzielczosc;
+    }
+}
+
+public class Aparat : Sprzet
+{
+    private string obiektyw;
+    private string matryca;
+
+    public Aparat(string obiektyw, string matryca)
+    {
+        id = id++;
+        typ = "aparat";
+        this.obiektyw =  obiektyw;
+        this.matryca =  matryca;
+    }
+}
+
+public class Laptop : Sprzet
+{
+    private int cale;
+    private string rozdzielczosc;
+
+    public Laptop(int cale, string rozdzielczosc)
+    {
+        id = id++;
+        typ = "laptop";
+        this.cale =  cale;
+        this.rozdzielczosc =  rozdzielczosc;
+    }
+}
